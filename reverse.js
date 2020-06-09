fca0a799921206ac2d6c2aec9dd57ccc7c7f3e24
yebaojia <695988558@qq.com>
2020-06-09 17:07:17 +0800

    递归

--- reverse.js
+++ reverse.js
@@ -3,17 +3,30 @@ var linkList = new LinkList();
 var linkList = new LinkList();
 linkList.appendBatch([1, 2, 3, 4, 5])
 
+// 双指针
+// var reverseList = function(head) {
+//   if (!head) {
+//       return head;
+//   }
+//   var curr = head, prev = null, tempNext = null;
+//   while (curr) {
+//       tempNext = curr.next;
+//       curr.next = prev;
+//       prev = curr;
+//       curr = tempNext;
+//   }
+//   return prev;
+// };
+
+// 递归
 var reverseList = function(head) {
-  if (!head) {
+  if (!head || !head.next) {
       return head;
   }
-  var curr = head, prev = null, tempNext = null;
-  while (curr) {
-      tempNext = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = tempNext;
-  }
-  return prev;
-};
+  var p = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return p
+}
+
 reverseList(linkList.head)
